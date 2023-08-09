49230f1b14fef91fc60b8de275ab47ad57db5c37
vlaship <vlaship@outlook.com>
2023-08-09 15:02:48 -0400

    person

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod person;
+
+use person::*;
+use person::people::People as ManyPersons;
+
+fn main() {
+    let mut users = vec!["Tom", "Sam", "Bob"];
+
+    let object: Object = Object{};
+
+    print_vec(&users);
+    users.push("Alice");
+
+    print_vec(&users);
+
+    println!("size {}", users.len());
+
+    print_vec(&users);
+
+    users.remove(2);
+
+    print_vec(&users);
+
+    println!("{}", users.pop().unwrap());
+
+    print_vec(&users);
+
+    let aa = Person {
+        first_name: "Tom".to_string(),
+        last_name: "Tom".to_string(),
+    };
+    let pep = ManyPersons {};
+}
+
+fn print_vec(users: &Vec<&str>) {
+    for user in users {
+        print!("{} ", user);
+    }
+    println!();
+}
---
+++ src/person.rs
@@ -0,0 +1,8 @@
+pub struct Person {
+    pub first_name: String,
+    pub last_name: String,
+}
+
+pub mod people {
+    pub struct People {}
+}
